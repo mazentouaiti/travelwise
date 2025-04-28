9a98d3624865198645068d58b71f07bcc665c4cc
Eya-Jmaa <eyaa.jmaa@gmail.com>
2025-04-28 14:28:39 +0100

    integration

--- src/main/java/com/example/travelwise/App.java
+++ src/main/java/com/example/travelwise/App.java
@@ -2,17 +2,28 @@ package com.example.travelwise;
 
 import com.example.travelwise.models.Model;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
-   /* @Override
-    public void start(Stage stage) {
-        Model.getInstance().getViewFactory().showLoginView();
-    }*/
+import java.io.IOException;
 
-    @Override
-    public void start(Stage stage) {
-        Model.getInstance().getViewFactory().showAdminView();
+
+    public class App extends Application {
+        @Override
+        public void start(Stage stage) throws IOException {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Fxml/Agences/agency_acc.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Accommodation ");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+
+
+        }
+
+        public static void main(String[] args) {
+            launch();
+        }
     }
-
-}
